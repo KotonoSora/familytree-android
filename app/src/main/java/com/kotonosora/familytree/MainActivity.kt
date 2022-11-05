0100d06653ff129e5f01ccb81ac6f25d8b042863
KotonoSora <reply@kotonosora.com>
2022-11-05 11:58:39 +0700

    feat(home-screen): UP-3 learn android compose lemonade app

--- app/src/main/java/com/kotonosora/familytree/MainActivity.kt
+++ app/src/main/java/com/kotonosora/familytree/MainActivity.kt
@@ -3,8 +3,12 @@ package com.kotonosora.familytree
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +16,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,8 +32,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    DiceRollerApp()
+                    LemonadeApp()
                 }
             }
         }
@@ -48,7 +52,6 @@ fun DefaultPreview() {
     }
 }
 
-@Preview
 @Composable
 fun DiceRollerApp() {
     DiceWithButtonAndImage(
@@ -58,7 +61,6 @@ fun DiceRollerApp() {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
@@ -76,5 +78,109 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerPreview() {
+    MyApplicationTheme {
+        DiceRollerApp()
+    }
+}
+
+@Composable
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    when (currentStep) {
+        1 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon_select,
+                drawableResourceId = R.drawable.lemon_tree,
+                contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                onImageClick = {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+            )
+        }
+        2 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon_squeeze,
+                drawableResourceId = R.drawable.lemon_squeeze,
+                contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                onImageClick = {
+                    squeezeCount--
+                    if (squeezeCount == 0) {
+                        currentStep = 3
+                    }
+                }
+            )
+        }
+        3 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon_drink,
+                drawableResourceId = R.drawable.lemon_drink,
+                contentDescriptionResourceId = R.string.lemonade_content_description,
+                onImageClick = {
+                    currentStep = 4
+                }
+            )
+        }
+        4 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.empty_glass_content_description,
+                drawableResourceId = R.drawable.lemon_restart,
+                contentDescriptionResourceId = R.string.empty_glass_content_description,
+                onImageClick = {
+                    currentStep = 1
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(stringResource(id = textLabelResourceId))
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = drawableResourceId),
+            contentDescription = stringResource(
+                id = contentDescriptionResourceId
+            ),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(onClick = onImageClick)
+                .border(
+                    BorderStroke(
+                        2.dp,
+                        Color(105, 205, 216)
+                    ),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadePreview() {
+    MyApplicationTheme {
+        LemonadeApp()
     }
 }
